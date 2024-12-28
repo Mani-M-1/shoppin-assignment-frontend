d76bb39de2b080fb4998188bdec21e27b40ec510
Manikanta Muppidi <128898564+Mani-M-1@users.noreply.github.com>
2024-12-28 10:38:12 +0530

    changes

--- src/components/ProductGallery/index.jsx
+++ src/components/ProductGallery/index.jsx
@@ -1,8 +1,9 @@
 import './index.css';
+import WatchImage from '/src/assets/images/watch-case-46-aluminum-jetblack-nc-s10_VW_PF+watch-face-46-aluminum-jetblack-s10_VW_PF.png';
 
 const ProductGallery = () => (
   <div className='gallery'>
-    <img src="/src/assets/images/watch-case-46-aluminum-jetblack-nc-s10_VW_PF+watch-face-46-aluminum-jetblack-s10_VW_PF.png" alt="Apple Watch" />
+    <img src={WatchImage} alt="Apple Watch" />
   </div>
 );
 
